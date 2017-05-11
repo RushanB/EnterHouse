43feb92e254e1692e8753cbaeeb2b739ff6c03db
RsuhanB <Rushanben@hotmail.com>
2017-05-10 23:02:56 -0400

    First Commit - Errors with Pictures

---
+++ EnterHouse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EnterHouse
+//
+//  Created by Rushan on 2017-05-10.
+//  Copyright © 2017 RushanBenazir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EnterHouse/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  EnterHouse
+//
+//  Created by Rushan on 2017-05-10.
+//  Copyright © 2017 RushanBenazir. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    //self.navigationItem.rightBarButtonItem;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
+
+
